c6eb5397d851b070b81beb99bb24e8dde0002024
Renan Santana <renan_ota@hotmail.com>
2021-09-13 15:35:01 -0300

    translate sentence

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
     class="custom-navbar justify-content-sm-center"
   >
     <b-nav-text>
-      Feito com <b-icon icon="heart-fill" variant="danger"></b-icon> (mas não muito) por <b-link href="https://github.com/renanstn">renanstn</b-link>
+      Made with <b-icon icon="heart-fill" variant="danger"></b-icon> (but not too much) by <b-link href="https://github.com/renanstn">renanstn</b-link>
     </b-nav-text>
   </b-navbar>
 </template>
